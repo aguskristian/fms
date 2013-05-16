8e3f27782ee4747dcf7f271e1989f891824a3599
I Made Agus Kristian <aguscristian21@gmail.com>
2013-05-15 20:35:43 -1200

    input_inventory

---
+++ application/views/inventory/inventory_edit_main.php
@@ -0,0 +1,122 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="../../calendar_js/jquery-ui-1.8.5.custom.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="../../calendar_js/jquery.ui.core.js"></script>
+<script type="text/javascript" src="../../calendar_js/jquery.ui.datepicker.js"></script>
+<script type="text/javascript" src="../../calendar_js/jquery.ui.widget.js"></script>
+<script type="text/javascript" src="../../calendar_js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="../../calendar_js/jquery-ui-1.8.5.custom.min.js"></script>
+<script type="text/javascript"> jQuery.noConflict() </script>
+<script>
+ jQuery(function(){
+	jQuery('.calender').datepicker({dateFormat:'yy-mm-dd',changeMonth:true,changeYear:true});
+
+});
+</script>
+
+
+<article class="module width_full">
+	<header><h3>Edit Data Inventory</h3></header>
+		<div class="module_content">
+           
+    		<table width="0" border="0" align="center" cellpadding="3" cellspacing="3" background="../../images/latar.jpg">
+        <?php echo form_open_multipart('controllersinventory/submit'); ?>
+ 		<?php echo form_hidden('id_inventory',$fid_inventory); ?>
+        
+        
+         <tr>
+         <td> <strong><?php echo form_label('ID Inventory  '); ?></strong></td>
+         <td> <?php echo form_input('id_inventory',$fid_inventory,'id="id_inventory"'); ?></td>
+         </tr> 
+         
+         <tr>
+         <td> <strong><?php echo form_label('Peralatan'); ?></strong></td>
+         <td> <?php echo form_input('peralatan',$fperalatan,'id="peralatan"'); ?></td>
+         </tr>   
+         
+         <tr>
+         <td> <strong><?php echo form_label('Type/Model'); ?></strong></td>
+         <td> <?php echo form_input('type_model',$ftype_model,'id="type_model"'); ?></td>
+         </tr>
+         
+         <tr>
+         <td> <strong><?php echo form_label('No. Seri'); ?></strong></td>
+         <td> <?php echo form_input('no_seri',$fno_seri,'id="no_seri"'); ?></td>
+         </tr>
+         
+         <tr>
+         <td> <strong><?php echo form_label('Tahun Perolehan'); ?></strong></td>
+         <td> <?php echo form_input('tahun_perolehan',$ftahun_perolehan,'id="tahun_perolehan"'); ?></td>
+         </tr>
+         
+          <tr>
+            <td ><strong>Unit</strong> </td>
+            <td > &nbsp;
+            	
+		<?php
+            #inisiasi data dropdown ke dalam array
+            $dropdown = array();
+        
+            #tambahkan data dropdown manual selain dari database
+            $dropdown['all'] = '--- Unit ---';
+            
+                            
+            if(!empty($query2))
+            {
+                  foreach($query2 as $row) 
+                    {
+                        $dropdown[$row->unit_nama] = $row->unit_nama;
+                    }
+            }
+                            
+            echo form_dropdown('unit_nama', $dropdown, 'all'); 
+        ?>
+            
+       	</tr>
+         
+         <tr>
+         <td> <strong><?php echo form_label('User'); ?></strong></td>
+         <td> <?php echo form_input('user',$fuser,'id="user"'); ?></td>
+         </tr>
+         
+         <tr>
+         <td> <strong><?php echo form_label('Main Trouble'); ?></strong></td>
+         <td> <?php echo form_input('main_trouble',$fmain_trouble,'id="main_trouble"'); ?></td>
+         </tr>
+         
+         <tr>
+         <td> <strong><?php echo form_label('date_trouble'); ?></strong></td>
+         <td> <?php echo form_input('date_trouble',$fdate_trouble,'id="date_trouble"'); ?></td>
+         </tr>
+         
+         <tr>
+         <td> <strong><?php echo form_label('SB'); ?></strong></td>
+         <td> <?php echo form_input('sb',$fsb,'id="sb"'); ?></td>
+         </tr>
+         
+         <tr>
+         <td> <strong><?php echo form_label('UR'); ?></strong></td>
+         <td> <?php echo form_input('ur',$fur,'id="ur"'); ?></td>
+         </tr>
+         
+         <tr>
+         <td> <strong><?php echo form_label('SCRP'); ?></strong></td>
+         <td> <?php echo form_input('scrp',$fscrp,'id="scrp"'); ?></td>
+         </tr>
+         
+         <tr>
+         <td> <strong><?php echo form_label('Remaks'); ?></strong></td>
+         <td> <?php echo form_input('remaks',$fremaks,'id="remaks"'); ?></td>
+         </tr>
+                
+         <tr>
+         <td></td>
+         <td> <?php    echo form_submit('submit','Submit','id="submit"'); echo form_close(); ?> </td>
+         </tr>
+ 
+</table>
+
+				<div class="clear"></div>
+	</div>
+</article><!-- end of stats article -->
+	
+    <div class="clear"></div>
---
+++ application/views/inventory/inventory_input_main.php
@@ -0,0 +1,124 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="../../calendar_js/jquery-ui-1.8.5.custom.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="../../calendar_js/jquery.ui.core.js"></script>
+<script type="text/javascript" src="../../calendar_js/jquery.ui.datepicker.js"></script>
+<script type="text/javascript" src="../../calendar_js/jquery.ui.widget.js"></script>
+<script type="text/javascript" src="../../calendar_js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="../../calendar_js/jquery-ui-1.8.5.custom.min.js"></script>
+<script type="text/javascript"> jQuery.noConflict() </script>
+<script>
+ jQuery(function(){
+	jQuery('.calender').datepicker({dateFormat:'yy-mm-dd',changeMonth:true,changeYear:true});
+
+});
+</script>
+
+
+<article class="module width_full">
+	<header><h3>Input Data Inventory </h3></header>
+		<div class="module_content">
+           
+    		<table width="0" border="0" align="center" cellpadding="3" cellspacing="3" background="../../images/latar.jpg">
+        
+		<?php echo form_open_multipart('controllersinventory/add_inventory'); ?>
+        
+        <tr>
+            <td> <strong>ID Inventory</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="id_inventory"/> </td>
+        </tr>
+         
+        <tr>
+            <td> <strong>Peralatan</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="peralatan"/> </td>
+        </tr>
+        
+        <tr>
+            <td> <strong>No. Inventory</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="no_inventory"/> </td>
+        </tr>
+        
+        <tr>
+            <td> <strong>Type/Model</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="type_model"/> </td>
+        </tr>
+        
+        <tr>
+            <td> <strong>No. Seri</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="no_seri"/> </td>
+        </tr>
+       
+        <tr>
+            <td> <strong>Tahun Perolehan</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="type_model" class="calender"/> </td>
+        </tr>
+        
+         <tr>
+            <td ><strong>Unit</strong> </td>
+            <td > &nbsp;
+            	
+		<?php
+            #inisiasi data dropdown ke dalam array
+            $dropdown = array();
+        
+            #tambahkan data dropdown manual selain dari database
+            $dropdown['all'] = '--- Unit ---';
+            
+                            
+            if(!empty($query2))
+            {
+                  foreach($query2 as $row) 
+                    {
+                        $dropdown[$row->unit_nama] = $row->unit_nama;
+                    }
+            }
+                            
+            echo form_dropdown('unit_nama', $dropdown, 'all'); 
+        ?>
+            
+       	</tr>
+        
+        <tr>
+            <td> <strong>User</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="user" /> </td>
+        </tr>
+         
+        <tr>
+            <td> <strong>Main Trouble</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="main_trouble"/> </td>
+        </tr>
+         
+        <tr>
+            <td> <strong>Date Trouble</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="date_trouble" class="calender"/> </td>
+        </tr>
+         
+        <tr>
+            <td> <strong>Kondisi Serviceable</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="sb"/> </td>
+        </tr>
+        <tr>
+        
+            <td> <strong>Kondisi UR</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="ur"/> </td>
+        </tr>
+        
+         <tr>
+            <td> <strong>Kondisi SCRP</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="scrp"/> </td>
+        </tr>
+        <tr>
+            <td> <strong>Remarks</strong> </td>
+            <td> &nbsp;&nbsp;<input type="text" name="remarks"/> </td>
+        </tr>
+         
+            <td></td>
+            <td colspan="2" align="left">&nbsp;&nbsp;<input type="submit" value="Add" > </td>
+        </tr>
+ 
+</table>
+
+				<div class="clear"></div>
+	</div>
+</article><!-- end of stats article -->
+	
+    <div class="clear"></div>
